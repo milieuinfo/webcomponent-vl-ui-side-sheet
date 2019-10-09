bedb87109965b79d0b86da76a94e4cce9ca7108f
zhaodifeng <zhaodi.feng@vlaanderen.be>
2019-10-09 09:43:10 +0200

    close button + dropback

--- vl-side-sheets.src.js
+++ vl-side-sheets.src.js
@@ -30,20 +30,39 @@ import {VlElement} from '/node_modules/vl-ui-core/vl-core.js';
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-side-sheets.html|Demo}
  *
  */
-export class VlSideSheets extends VlElement(HTMLElement){
+export class VlSideSheets extends VlElement(HTMLElement) {
   constructor() {
     super(`
         <style> 
         @import "../style.css";
-        </style>
+        </style>       
+        <div id="vl-side-sheets-backdrop"></div>
         <div id="vl-side-sheets">
+         <button type="button" class="vl-side-sheets__close">
+            <i class="vl-side-sheets__close__icon vl-vi vl-vi-cross" aria-hidden="true"></i>
+            <span class="vl-u-visually-hidden">Venster sluiten</span>
+          </button>
           <slot></slot>
         </div>
         `);
   }
 
+  connectedCallback(){
+    this._closeButton.addEventListener("click",(e)=>{
+      this.close();
+    });
+  }
+
+  get _closeButton() {
+    return this._shadow.querySelector('.vl-side-sheets__close');
+  }
+
   get _sheetsElement() {
-    return this._element;
+    return this._shadow.querySelector('#vl-side-sheets');
+  }
+
+  get _backdropElement() {
+    return this._shadow.querySelector('#vl-side-sheets-backdrop');
   }
 
   toggle() {
@@ -51,15 +70,17 @@ export class VlSideSheets extends VlElement(HTMLElement){
   }
 
   open() {
-    this._sheetsElement.classList.add('active');
+    this._sheetsElement.setAttribute('open', '');
+    this._backdropElement.setAttribute('open', '');
   }
 
   close() {
-    this._sheetsElement.classList.remove('active');
+    this._sheetsElement.removeAttribute('open');
+    this._backdropElement.removeAttribute('open');
   }
 
   get isOpen() {
-    return this._element.classList.contains('active');
+    return this._element.hasAttribute('open');
   }
 }
 
