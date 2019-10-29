e20ef158102ec4c3dddf4e33533145c48e512283
Tom Coemans <tom.coemans@gmail.com>
2019-10-29 10:39:39 +0100

    minor improvements

--- vl-side-sheet.src.js
+++ vl-side-sheet.src.js
@@ -1,21 +1,5 @@
-import {VlElement,define} from '/node_modules/vl-ui-core/vl-core.js';
-import {SwipeDetect} from '../dist/swipeDetect.js';
-
-(() => {
-  loadScript('util',
-      '/node_modules/@govflanders/vl-ui-util/dist/js/util.min.js', () => {
-      });
-
-  function loadScript(id, src, onload) {
-    if (!document.head.querySelector('#' + id)) {
-      let script = document.createElement('script');
-      script.setAttribute('id', id);
-      script.setAttribute('src', src);
-      script.onload = onload;
-      document.head.appendChild(script);
-    }
-  }
-})();
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+import { SwipeDetect } from '../dist/swipeDetect.js';
 
 /**
  * VlSide-sheet
@@ -25,6 +9,7 @@ import {SwipeDetect} from '../dist/swipeDetect.js';
  * @extends VlElement
  *
  * @property {boolean} left - Attribute wordt gebruikt om aan te duiden dat de side-sheet de linkererand van het scherm moet plaatsen.
+ * @property {boolean} right - Attribute wordt gebruikt om aan te duiden dat de side-sheet de rechterkant van het scherm moet plaatsen.
  * @property {boolean} enable-swipe - Attribute wordt gebruikt om aan te duiden dat swipe functie toegelaten is.
  *
  * @example Breedte van de side sheet aanpassen(op grote scherm):
@@ -44,29 +29,31 @@ import {SwipeDetect} from '../dist/swipeDetect.js';
  */
 export class VlSidesheet extends VlElement(HTMLElement) {
   static get _observedAttributes() {
-    return ['left','enable-swipe'];
+    return ['left', 'right', 'enable-swipe'];
   }
 
   constructor() {
     super(`
-        <style> 
+      <style> 
         @import "../style.css";
-        </style>       
-        <div id="vl-side-sheet-backdrop"></div>
-        <div id="vl-side-sheet">
-         <button type="button" class="vl-side-sheet__close">
-            <i class="vl-side-sheet__close__icon vl-vi vl-vi-cross" aria-hidden="true"></i>
-            <span class="vl-u-visually-hidden">Venster sluiten</span>
-         </button>
-         <slot></slot>
-        </div>
-        `);
+      </style>  
+      <div id="vl-side-sheet-backdrop"></div>
+      <div id="vl-side-sheet">
+        <button type="button" class="vl-side-sheet__close">
+          <i class="vl-side-sheet__close__icon vl-vi vl-vi-cross" aria-hidden="true"></i>
+          <span class="vl-u-visually-hidden">Venster sluiten</span>
+        </button>
+        <slot></slot>
+      </div>
+    `);
   }
 
   connectedCallback() {
-    this._closeButton.addEventListener("click", (e) => {
-      this.close();
-    });
+    this._closeButton.addEventListener("click", () => this.close());
+  }
+
+  get isOpen() {
+    return this._element.hasAttribute('open');
   }
 
   get _closeButton() {
@@ -81,50 +68,52 @@ export class VlSidesheet extends VlElement(HTMLElement) {
     return this._shadow.querySelector('#vl-side-sheet-backdrop');
   }
 
-  /*
-   * Toggle
+  /**
+   * Triggert een toggle van de side-sheet zonder te klikken op de side-sheet.
+   * 
+   * @Return {void}
    */
   toggle() {
     this.isOpen ? this.close() : this.open();
   }
 
-  /*
+  /**
    * Handmatig openen van de side-sheet
+   * 
+   * @Return {void}
    */
   open() {
     this._sheetElement.setAttribute('open', '');
     this._backdropElement.setAttribute('open', '');
   }
 
-  /*
+  /**
    * Handmatig sluiten van de side-sheet
+   * 
+   * @Return {void}
    */
   close() {
     this._sheetElement.removeAttribute('open');
     this._backdropElement.removeAttribute('open');
-    if(this._onClose){
+    if (this._onClose) {
       this._onClose();
     }
   }
 
-  /*
+  /**
    * De callback wordt uitgevoerd direct na de afsluiten van een side sheet.
-   * @param event
-   * @param callback
+   * 
+   * @param {function} callback 
    */
-  onClose(callBack) {
-    this._onClose = callBack;
-  }
-
-  get isOpen() {
-    return this._element.hasAttribute('open');
+  onClose(callback) {
+    this._onClose = callback;
   }
 
   _leftChangedCallback(oldValue, newValue) {
     if (newValue !== undefined) {
-      this._sheetElement.setAttribute('data-vl-side-sheet-left', '')
+      this._sheetElement.classList.add('vl-side-sheet--left');
     } else {
-      this._sheetElement.remove('data-vl-side-sheet-left');
+      this._sheetElement.classList.remove('vl-side-sheet--left');
     }
   }
 
