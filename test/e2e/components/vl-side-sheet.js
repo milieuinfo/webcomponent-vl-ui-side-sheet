cdba2ac2e834b7697aaa3feba9a105c1adbfe6f6
cambiph <incubusattax@gmail.com>
2020-01-23 16:13:21 +0100

    Extra E2E testen

--- test/e2e/components/vl-side-sheet.js
+++ test/e2e/components/vl-side-sheet.js
@@ -1,6 +1,26 @@
 const { VlElement } = require('vl-ui-core');
+const { By } = require('selenium-webdriver');
 
 class VlSideSheet extends VlElement {  
+    async _getSideSheet() {
+        return this.shadowRoot;
+    }
+
+    async _getCloseButton() {
+        return (await this._getSideSheet()).findElement(By.css('button.vl-side-sheet__close'));
+    }
+
+    async isOpen() {
+        return (await this._getSideSheet()).hasAttribute('open');
+    }
+
+    async close() {
+        return (await this._getCloseButton()).click();
+    }
+
+    async getContent() {
+        return (await this._getSideSheet()).getText();
+    }
 }
 
 module.exports = VlSideSheet;
--- test/e2e/pages/vl-side-sheet.page.js
+++ test/e2e/pages/vl-side-sheet.page.js
@@ -1,10 +1,19 @@
 const VlSideSheet = require('../components/vl-side-sheet');
 const { Page } = require('vl-ui-core');
 const { Config } = require('vl-ui-core');
+const { By } = require('selenium-webdriver');
 
 class VlSideSheetPage extends Page {
     async _getSideSheet(selector) {
         return new VlSideSheet(this.driver, selector);
+    }
+
+    async open() {
+        return (await this._getSideSheet('#open-button')).click();
+    }
+
+    async getSidesheetToggle() {
+        return this._getSideSheet('#vl-side-sheet-1');
     }
 
     async load() {
