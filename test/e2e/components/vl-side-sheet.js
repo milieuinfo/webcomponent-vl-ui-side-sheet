7700f58388e5175a5e27d7ec0462f9f771967f25
coemans <tom.coemans@gmail.com>
2020-06-03 16:44:22 +0200

    Introductie vl-ui-demo en ESlint (#33)

--- test/e2e/components/vl-side-sheet.js
+++ test/e2e/components/vl-side-sheet.js
@@ -1,37 +1,36 @@
-const { VlElement } = require('vl-ui-core').Test;
-const { By } = require('selenium-webdriver');
+const {VlElement} = require('vl-ui-core').Test;
+const {By} = require('selenium-webdriver');
 
-class VlSideSheet extends VlElement {  
+class VlSideSheet extends VlElement {
+  async _getCloseButton() {
+    return this.shadowRoot.findElement(By.css('button.vl-side-sheet__close'));
+  }
 
-    async _getCloseButton() {
-        return this.shadowRoot.findElement(By.css('button.vl-side-sheet__close'));
-    }
+  async isOpen() {
+    return this.shadowRoot.hasAttribute('open');
+  }
 
-    async isOpen() {
-        return this.shadowRoot.hasAttribute('open');
-    }
+  async isLeft() {
+    return this.hasAttribute('left');
+  }
 
-    async isLeft() {
-    	return this.hasAttribute('left');
-    }
+  async isSwipeEnabled() {
+    return this.hasAttribute('enable-swipe');
+  }
 
-    async isSwipeEnabled() {
-    	return this.hasAttribute('enable-swipe');
-    }
-    
-    async close() {
-    	const closeButton = await this._getCloseButton();
-        return closeButton.click();
-    }
-    
-    async getContentSlotNodes() {
-    	const contentSlot = await this._getContentSlot();
-    	return this.getAssignedNodes(contentSlot);
-    }
+  async close() {
+    const closeButton = await this._getCloseButton();
+    return closeButton.click();
+  }
 
-    async _getContentSlot() {
-        return this.shadowRoot.findElement(By.css('#vl-side-sheet slot'));
-    }
+  async getContentSlotNodes() {
+    const contentSlot = await this._getContentSlot();
+    return this.getAssignedNodes(contentSlot);
+  }
+
+  async _getContentSlot() {
+    return this.shadowRoot.findElement(By.css('#vl-side-sheet slot'));
+  }
 }
 
 module.exports = VlSideSheet;
--- test/e2e/pages/vl-side-sheet.page.js
+++ test/e2e/pages/vl-side-sheet.page.js
@@ -1,66 +1,66 @@
 const VlSideSheet = require('../components/vl-side-sheet');
-const { Page, Config } = require('vl-ui-core').Test;
-const { By } = require('selenium-webdriver');
-const { VlButton } = require('vl-ui-button').Test;
-const { VlDatepicker } = require('vl-ui-datepicker').Test;
+const {Page, Config} = require('vl-ui-core').Test;
+const {By} = require('selenium-webdriver');
+const {VlButton} = require('vl-ui-button').Test;
+const {VlDatepicker} = require('vl-ui-datepicker').Test;
 
 class VlSideSheetPage extends Page {
-    async _getSideSheet(selector) {
-        return new VlSideSheet(this.driver, selector);
-    }
+  async _getSideSheet(selector) {
+    return new VlSideSheet(this.driver, selector);
+  }
 
-    async _getButton(selector) {
-    	return new VlButton(this.driver, this.driver.findElement(By.css(selector)))
-    }
-    
-    async toggleSidesheet() {
-       	const openSideSheetButton = await this._getButton('#vl-side-sheet-toggle-button');
-        return openSideSheetButton.click();
-    }
-    
-    async openSidesheet() {
-    	const openSideSheetButton = await this._getButton('#vl-side-sheet-open-button');
-    	return openSideSheetButton.click();
-    }
-    
-    async closeSidesheet() {
-    	const openSideSheetButton = await this._getButton('#vl-side-sheet-close-button');
-    	return openSideSheetButton.click();
-    }
-    
-    async openSidesheetWithCloseListener() {
-    	const openSideSheetButton = await this.getOpenSidesheetWithCloseListenerButton();
-    	return openSideSheetButton.click();
-    }
-    
-    async getOpenSidesheetWithCloseListenerButton() {
-    	return await this._getButton('#vl-side-sheet-open-button-with-close-listener');
-    }
-    
-    async getSidesheet() {
-        return this._getSideSheet('#vl-side-sheet');
-    }
+  async _getButton(selector) {
+    return new VlButton(this.driver, this.driver.findElement(By.css(selector)));
+  }
 
-    async getSidesheetLeft() {
-    	return this._getSideSheet('#vl-side-sheet-left');
-    }
+  async toggleSidesheet() {
+    const openSideSheetButton = await this._getButton('#vl-side-sheet-toggle-button');
+    return openSideSheetButton.click();
+  }
 
-    async clickOpenSideSheetButton() {
-        const button = await this._getButton('#side-sheet-with-datepicker-open-button');
-        await button.click();
-    }
+  async openSidesheet() {
+    const openSideSheetButton = await this._getButton('#vl-side-sheet-open-button');
+    return openSideSheetButton.click();
+  }
 
-    async getSidesheetDatepicker() {
-        return this._getDatepicker('#vl-datepicker');
-    }
+  async closeSidesheet() {
+    const openSideSheetButton = await this._getButton('#vl-side-sheet-close-button');
+    return openSideSheetButton.click();
+  }
 
-    async _getDatepicker(selector) {
-        return new VlDatepicker(this.driver, selector);
-    }
+  async openSidesheetWithCloseListener() {
+    const openSideSheetButton = await this.getOpenSidesheetWithCloseListenerButton();
+    return openSideSheetButton.click();
+  }
 
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-side-sheet.html');
-    }
+  async getOpenSidesheetWithCloseListenerButton() {
+    return await this._getButton('#vl-side-sheet-open-button-with-close-listener');
+  }
+
+  async getSidesheet() {
+    return this._getSideSheet('#vl-side-sheet');
+  }
+
+  async getSidesheetLeft() {
+    return this._getSideSheet('#vl-side-sheet-left');
+  }
+
+  async clickOpenSideSheetButton() {
+    const button = await this._getButton('#side-sheet-with-datepicker-open-button');
+    await button.click();
+  }
+
+  async getSidesheetDatepicker() {
+    return this._getDatepicker('#vl-datepicker');
+  }
+
+  async _getDatepicker(selector) {
+    return new VlDatepicker(this.driver, selector);
+  }
+
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-side-sheet.html');
+  }
 }
 
 module.exports = VlSideSheetPage;
